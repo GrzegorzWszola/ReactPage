d368da44b51bf8a63fed33214ebd28d423194a7a
Grzegorz Wszoła <grzegorzwszola1@gmail.com>
2023-12-22 16:58:15 +0100

    Some changes in Home element

--- src/Elements/Home/Home.jsx
+++ src/Elements/Home/Home.jsx
@@ -1,4 +1,6 @@
 import React from "react"
+import { Canvas } from '@react-three/fiber'
+
 
 import './Home.css'
 
@@ -8,7 +10,7 @@ const Home = () => {
     return(
         <div className="Home" id="home">
             <div className="Home_Text">
-                <div className="Home_Main">Hi I'm ...</div>
+                <div className="Home_Main">Hi I'm *Name*</div>
                 <div className="Home_Subtext">Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Modi ullam, tempore blanditiis veritatis eum odio! Asperiores vero odit
                     vitae officia accusantium. Doloremque porro exercitationem sunt est,
@@ -21,7 +23,7 @@ const Home = () => {
                 </div>
             </div>
             <div className="Home_Model">
-               
+                <script type="module" src="./3DModel.jsx"></script>
             </div>
         </div>
     )
